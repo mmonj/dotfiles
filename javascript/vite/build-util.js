ef0c2319b8a14695291f8fe5b92c62207eaea3ad
mmonj <mmonj@users.noreply.github.com>
2024-01-26 21:36:58 -0500

    add Vite build script

---
+++ javascript/vite/build-util.js
@@ -0,0 +1,94 @@
+import fs from "node:fs";
+import path from "node:path";
+
+// ts file names that end with this string will be targeted for transpilation/bundling
+const ENTRYPOINT_IDENTIFIER = ".user.ts";
+
+/**
+ *
+ * @param {string} directoryPath
+ * @returns {string[]}
+ */
+export function getUserscriptPaths(directoryPath) {
+  /**@type {string[]} */
+  const userscriptsFilePaths = [];
+
+  const files = fs.readdirSync(directoryPath);
+
+  files.forEach((file) => {
+    if (file.endsWith(ENTRYPOINT_IDENTIFIER)) {
+      const filePath = path.join(directoryPath, file);
+      userscriptsFilePaths.push(filePath);
+    }
+  });
+
+  return userscriptsFilePaths;
+}
+
+/**
+ * Retain comments included at the top of the file (series of // comments). Useful for *monkey userscripts
+ * @param {string} tsFilePath
+ * @returns {string[]}
+ */
+export function parseInitialComments(tsFilePath) {
+  /**@type {string[]} */
+  const commentsList = [];
+
+  const content = fs.readFileSync(tsFilePath, "utf8");
+
+  const lines = content.split("\n");
+  for (const line of lines) {
+    if (line.trim().startsWith("//")) {
+      commentsList.push(line.trim());
+    } else {
+      return commentsList;
+    }
+  }
+
+  return commentsList;
+}
+
+/**
+ *
+ * @param {string} jsFilePath
+ * @param {string[]} comments
+ * @returns {void}
+ */
+export function prependCommentsToJsFiles(jsFilePath, comments) {
+  if (comments.length === 0) {
+    return;
+  }
+
+  try {
+    const jsFileContent = fs.readFileSync(jsFilePath, "utf8");
+    const newContent = comments.join("\n") + "\n\n" + jsFileContent;
+
+    fs.writeFileSync(jsFilePath, newContent, "utf8");
+    console.log(`Comments prepended to ${jsFilePath} successfully.`);
+  } catch (error) {
+    console.error(`Error writing to file: ${jsFilePath}`);
+    console.error(error);
+  }
+}
+
+/**
+ *
+ * @param {string} inputString
+ * @param {string} prefix
+ * @returns {string}
+ */
+export function slugifyAndCapitalize(inputString, prefix) {
+  if (inputString.length === 0) {
+    throw new Error("The input string is length 0");
+  }
+
+  const prefixRegex = /^\w*$/g;
+  if (!prefixRegex.test(prefix)) {
+    throw new Error("Prefix can only contain alphanumeric characters");
+  }
+
+  const dunderPrefix = prefix.length === 0 ? "" : `__${prefix}_`;
+  const slugifiedString = inputString.replaceAll(/[^A-Za-z]+/g, "_");
+
+  return `${dunderPrefix}${slugifiedString}__`.toUpperCase();
+}
---
+++ javascript/vite/build.js
@@ -0,0 +1,65 @@
+/*
+  Build script for Vite in order to bundle multiple scripts to UMD
+  Made to work around the following Vite restriction: 
+    Multiple entry points are not supported when output formats include "umd" or "iife".
+*/
+
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { build } from "vite";
+import {
+  getUserscriptPaths,
+  prependCommentsToJsFiles,
+  slugifyAndCapitalize,
+} from "./build-util.js";
+
+/**
+ *
+ * @param {string} inFile
+ * @param {string} buildDir
+ * @param {string} jsOutputName
+ */
+async function buildBundle(inFile, buildDir, jsOutputName) {
+  const currentFileUrl = import.meta.url;
+  const currentFilePath = fileURLToPath(currentFileUrl);
+
+  try {
+    await build({
+      configFile: false,
+      // root: "./",
+      build: {
+        emptyOutDir: false,
+        minify: false,
+        outDir: buildDir,
+        sourcemap: false,
+        target: "esnext",
+        lib: {
+          entry: inFile,
+          name: slugifyAndCapitalize(jsOutputName, "MM"),
+          fileName: () => jsOutputName,
+          // fileName: (format, entryName) => `${entryName}.js`,
+          formats: ["umd"],
+        },
+      },
+      resolve: {
+        alias: {
+          "@lib": currentFilePath,
+        },
+      },
+    });
+  } catch (error) {
+    console.error("Build failed:", error);
+  }
+  console.log("Build successful!");
+}
+
+getUserscriptPaths("./src").forEach(async (tsFilePath) => {
+  const buildDir = "./dist/prod";
+
+  const jsOutputName = path.basename(tsFilePath).replace(/\.ts$/, ".js");
+  // const comments = parseInitialComments(tsFilePath);
+  await buildBundle(tsFilePath, buildDir, jsOutputName);
+  setTimeout(() => {
+    prependCommentsToJsFiles(path.join(buildDir, jsOutputName), []);
+  }, 500);
+});
